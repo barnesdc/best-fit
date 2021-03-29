c43a7c2541a283d44ee8527190859a235d601fcc
Dante Barnes <barnes.dante.91@gmail.com>
2021-03-28 22:54:42 -0400

    remove exposed credentials

--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors())
 
 // https://www.mongodb.com/cloud/atlas
-const MONGO_URI = 'mongodb+srv://bestfitadmin:bestfitadmin1234@cluster0.aih8p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const MONGO_USER=  process.env.MONGO_USER
+const MONGO_PASS = process.env.MONGO_PASS
+const MONGO_URI = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster0.aih8p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
